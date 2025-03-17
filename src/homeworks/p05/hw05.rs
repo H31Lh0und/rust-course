c65102f32b71071e36f09c81d2a800e1bde8a3ed
H31Lh0und <darkset030@gmail.com>
2025-03-17 18:20:41 +0200

    hw05.rs

--- src/homeworks/p05/hw05.rs
+++ src/homeworks/p05/hw05.rs
@@ -7,9 +7,28 @@ fn gcd(mut a: u32, mut b: u32) -> u32 {
     a
 }
 
-fn main() {
-    let num1 = 56;
-    let num2 = 98;
-    
-    println!("GCD({}, {}) = {}", num1, num2, gcd(num1, num2));
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let data = [
+            ((42, 56), 14),
+            ((27, 18), 9),
+            ((81, 63), 9),
+            ((144, 48), 48),
+            ((35, 25), 5),
+            ((121, 11), 11),
+            ((99, 33), 33),
+            ((77, 22), 11),
+            ((55, 20), 5),
+            ((91, 13), 1),
+            ((132, 96), 12),
+        ];
+
+        for ((a, b), exp) in data.iter() {
+            assert_eq!(*exp, gcd(*a, *b));
+        }
+    }
 }
